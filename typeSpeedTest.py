eb68e0a8a1082c72a8befc40d665aa9338e5069f
adamwright <adamjwright@rocketmail.com>
2020-11-04 17:52:52 +0000

    getWords func, new txt
    reads all words from file, returns them as tuple, closes file. also deleted original 200words file and made into 1 line, for easier use. new file called 200words1line

--- typeSpeedTest.py
+++ typeSpeedTest.py
@@ -1,33 +1,53 @@
 #importing all libraries
+from os import close
 import random, tkinter
 
-#initialising the tkinter interpreter (would be done anyway after the first widget, but useful)
-root= tkinter.Tk()
 
-#creates the canvas
-window = tkinter.Canvas(root, width = 400, height = 300)
-window.pack()
 
-typeBox = tkinter.Entry (root)
-window.create_window(200, 140, window=typeBox)
-
-#taking the input from the typebox
-userInput = typeBox.get()
+#open the text file with words in
+def getWords():
+    #declares tuple var as list so it can be appended to
+    words200Tuple = []
+    #opens text file with all words
+    words200 = open("200words1line.txt", "r")
+    #reads and 
+    words200Tuple = str((words200.readline())).split()
+    words200Tuple = tuple(words200Tuple)
+    words200.close()
+    return(words200Tuple)
 
 
 #a function for when the user chooses to start the test
-def start():
-    pass
+def run():
+    print("the test would be starting")
 
 
-#some text on the window
-text1 = tkinter.Label(root, text= userInput)
-#a declaration of where the text is in the window
-window.create_window(200, 230, window=text1)
+def window():
+    #initialising the tkinter interpreter (would be done anyway after the first widget, but useful)
+    root= tkinter.Tk()
 
-#a button
-button = tkinter.Button(text='Start typing test', command=start)
-#a declaration of where the button is in the window
-window.create_window(200, 180, window=button)
+    #creates the canvas
+    window = tkinter.Canvas(root, width = 400, height = 300)
+    window.pack()
 
-root.mainloop()
+    typeBox = tkinter.Entry (root)
+    window.create_window(200, 140, window=typeBox)
+
+    #taking the input from the typebox
+    userInput = typeBox.get()
+
+
+    #some text on the window
+    text1 = tkinter.Label(root, text= userInput)
+    #a declaration of where the text is in the window
+    window.create_window(200, 230, window=text1)
+
+    #a button
+    button = tkinter.Button(text='Start typing test', command=run)
+    #a declaration of where the button is in the window
+    window.create_window(200, 180, window=button)
+    root.mainloop()
+
+words = getWords()
+print(words)
+
