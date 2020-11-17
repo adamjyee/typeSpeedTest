16389e665cbffd96612d6e5a933251e629fef1cf
adamwright <adamjwright@rocketmail.com>
2020-11-17 00:55:38 +0000

    comments, fixed wordGen(global:/), changed colour of text1

--- typeSpeedTest.py
+++ typeSpeedTest.py
@@ -9,15 +9,22 @@ def getWords():
     words200Tuple = []
     #opens text file with all words
     words200 = open("200words1line.txt", "r")
-    #reads and 
-    words200Tuple = str((words200.readline())).split()
+    #reads from file and changes it into a string, and then a list
+    words200Tuple = (str((words200.readline()))).split()
+    #changes to a tuple so things wont be messed with
     words200Tuple = tuple(words200Tuple)
     words200.close()
     return(words200Tuple)
 
 
+#creates a random list of the the tuple read from the file in the func above
+#this is unfortunately made into a global but because it's immutable it's not as bad
 def wordGenerator(wordTuple):
-    return wordTuple[random.randint(0,len(wordTuple))]
+    randomWordTuple = []
+    for x in range(300):
+        randomWordTuple.append(wordTuple[random.randint(0,(len(wordTuple)-1))])
+    tuple(randomWordTuple)
+    return randomWordTuple
 
 
 #a function for when the user chooses to start the test
@@ -27,12 +34,15 @@ def run():
 
 #a test function to output when a key has been pressed
 #has to have arg event otherwise outputs an error
-#the key that has been pressed is in the char part of the "event" var
+#the key that has been pressed is in the char part of the "event" var (can be called with keysym)
 def keyPress(event):
     print("a key has been pressed")
     #returns the key that's been pressed
     return(event.keysym)
 
+
+#this is the equivalent of the main function, it makes the app and uses mainloop() to run
+#
 def app():
     print("running app")
     #initialising the tkinter interpreter (would be done anyway after the first widget, but useful)
@@ -45,11 +55,9 @@ def app():
     typeBox = tkinter.Entry (root)
     app.create_window(200, 140, window=typeBox)
 
-    #taking the input from the typebox
-    userInput = typeBox.get()
-
     #shows the first few words for test, taken from the getWords function
-    text1 = tkinter.Label(root, text= getWords()[0:8])
+    text1 = tkinter.Label(root, fg="green", text= randomWords[0:8])
+
     #a declaration of where the text is in the window
     app.create_window(200, 100, window= text1)
 
@@ -64,4 +72,5 @@ def app():
 
     root.mainloop()
 
+randomWords = tuple(wordGenerator(getWords()))
 app()
